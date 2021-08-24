3130ea8b3c9f5040c6603494362d99657a030199
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-08-24 17:57:01 +0530

    Updated App.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
-<template></template>
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
 
-<script setup>
+<script>
+import { watch } from "@vue/runtime-core";
+import { useRoute } from "vue-router";
+export default {
+  setup() {
+    const route = useRoute();
+
+    watch(route, () => {
+      const app = document.getElementById("app");
+      if (app) app.style.overflowY = "hidden";
+      setTimeout(() => {
+        if (app) app.style.overflowY = "auto";
+      }, 1000);
+    });
+  },
+};
 </script>
 
 <style>
+#app {
+  height: 100vh;
+  overflow-x: hidden;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VWave from "v-wave";
+import "./components/lib/styles.css";
+import "vue3-circle-progress/dist/circle-progress.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(VWave);
+app.mount("#app");
