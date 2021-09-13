0d2a1080bea34ee3040abfdca3c2dcfbb318c819
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-09-13 20:45:03 +0530

    Added Sentry error tracking

--- src/main.js
+++ src/main.js
@@ -5,8 +5,22 @@ import store from "./store";
 import VWave from "v-wave";
 import "./components/lib/styles.css";
 import "vue3-circle-progress/dist/circle-progress.css";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+import { sentry as sentryConfig } from "./utils/constants";
 
 const app = createApp(App);
+Sentry.init({
+  app,
+  dsn: sentryConfig.dsn,
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: sentryConfig.tracingOrigins,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 app.use(router);
 app.use(store);
 app.use(VWave);
