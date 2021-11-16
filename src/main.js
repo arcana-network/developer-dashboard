ad7dcd485a7d74c0b534864e22896c955ff43577
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-11-16 15:28:11 +0530

    Removed polyfills and uncommented sentry

--- src/main.js
+++ src/main.js
@@ -8,25 +8,19 @@ import "vue3-circle-progress/dist/circle-progress.css";
 import * as Sentry from "@sentry/vue";
 import { Integrations } from "@sentry/tracing";
 import { sentry as sentryConfig } from "./utils/constants";
-import { Buffer } from "buffer";
-import { Stream } from "stream-browserify";
-
-window.global = window;
-window.Buffer = window.Buffer || Buffer;
-window.Stream = Stream;
 
 const app = createApp(App);
-// Sentry.init({
-//   app,
-//   dsn: sentryConfig.dsn,
-//   integrations: [
-//     new Integrations.BrowserTracing({
-//       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-//       tracingOrigins: sentryConfig.tracingOrigins,
-//     }),
-//   ],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  app,
+  dsn: sentryConfig.dsn,
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: sentryConfig.tracingOrigins,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 app.use(router);
 app.use(store);
 app.use(VWave);
