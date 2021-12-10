6a506a51a6a2684ca05340c8f0e40bda3e95aaf9
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-12-10 15:42:09 +0530

    Added group and portal id to env

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -59,11 +59,14 @@ export function login({ signature, email, address }) {
 }
 
 export function addUserToMailchimp(email) {
+  const mailchimpGroup = import.meta.env.VITE_MAILCHIMP_GROUP;
+  const mailchimpDevPortalId = import.meta.env.VITE_MAILCHIMP_DEV_PORTAL_ID;
+
   const data = {
     u: import.meta.env.VITE_MAILCHIMP_USER_ID,
     id: import.meta.env.VITE_MAILCHIMP_LIST_ID,
     EMAIL: email,
-    "group[381006][8]": 8,
+    [`${mailchimpGroup}[${mailchimpDevPortalId}]`]: mailchimpDevPortalId,
   };
 
   const stringifiedData = new URLSearchParams(data).toString();
