5dd882d7fdc8b84e5ec1dd43e8fbaf13afd39c96
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-02-04 13:50:42 +0530

    Modified get env api to add versions to api
    1. Added version parameter that accepts string or false
    2. If version is not supplied, it fallbacks to current version
    3. If version is false, then it skips api versioning and returns unversioned raw api

--- src/services/get-env-api.js
+++ src/services/get-env-api.js
@@ -1,8 +1,24 @@
 import store from "../store";
 import constants from "../utils/constants";
 
-export default function () {
-  return store.getters.env === "test"
-    ? constants.api.testnet
-    : constants.api.mainnet;
+const CURRENT_API_VERSION = import.meta.env.VITE_CURRENT_API_VERSION;
+
+export default function (version) {
+  let apiEndpoint =
+    store.getters.env === "test"
+      ? constants.api.testnet
+      : constants.api.mainnet;
+
+  if (version === false) {
+    return apiEndpoint;
+  }
+
+  if (typeof version !== "string" || !version.trim()) {
+    version = CURRENT_API_VERSION;
+  }
+
+  if (!apiEndpoint.endsWith("/")) {
+    apiEndpoint += "/";
+  }
+  return apiEndpoint + "api/" + version;
 }
