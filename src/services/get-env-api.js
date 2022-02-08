1fad07e217dd99042187c56e8797e60a0884d963
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-02-08 12:43:04 +0530

    Named export for getEnvApi

--- src/services/get-env-api.js
+++ src/services/get-env-api.js
@@ -3,7 +3,7 @@ import constants from "../utils/constants";
 
 const CURRENT_API_VERSION = import.meta.env.VITE_CURRENT_API_VERSION;
 
-export default function (version = CURRENT_API_VERSION) {
+export default function getEnvApi(version = CURRENT_API_VERSION) {
   let apiEndpoint =
     store.getters.env === "test"
       ? constants.api.testnet
