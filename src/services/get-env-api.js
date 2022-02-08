af7fd95fc16d80596b8007a29a387416d195c391
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-02-08 12:40:52 +0530

    Added default version if version is not passed

--- src/services/get-env-api.js
+++ src/services/get-env-api.js
@@ -3,7 +3,7 @@ import constants from "../utils/constants";
 
 const CURRENT_API_VERSION = import.meta.env.VITE_CURRENT_API_VERSION;
 
-export default function (version) {
+export default function (version = CURRENT_API_VERSION) {
   let apiEndpoint =
     store.getters.env === "test"
       ? constants.api.testnet
@@ -11,10 +11,6 @@ export default function (version) {
 
   if (version === false) {
     return apiEndpoint;
-  }
-
-  if (typeof version !== "string" || !version.trim()) {
-    version = CURRENT_API_VERSION;
   }
 
   if (!apiEndpoint.endsWith("/")) {
