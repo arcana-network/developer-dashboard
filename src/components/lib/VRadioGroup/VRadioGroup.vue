15a111a110a62c94ae937e80caa9f6e199cc1d05
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-02-10 15:17:12 +0530

    create VRadioGroup component with VRadio component

---
+++ src/components/lib/VRadioGroup/VRadioGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-radio
+    v-for="option in options"
+    :key="option.value"
+    :value="option.value"
+    :label="option.label"
+    :name="name"
+    :selectedValue="selectedChain"
+    @update:modelValue="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script>
+import VRadio from "../VRadio/VRadio.vue";
+
+export default {
+  components: { VRadio },
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    selectedChain: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
