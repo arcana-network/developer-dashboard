d85ac62c594c3a22dab19eb1fff13125a2b2bd08
KarthikSiddarth <iamkarthiksiddarth@gmail.com>
2022-02-11 14:28:50 +0530

    [AR-1023] Create Radio Button Component (#50)
    * create Radio button component
    
    * create radio button group component
    
    * move into lib dir
    
    * bind "for" attr to label, "id" attr to input el
    
    * use modelValue to bind selected value from parent
    
    * add emits prop
    
    * change data types value to Str, Num, Bool

---
+++ src/components/lib/VRadioGroup/VRadioGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-radio
+    v-for="option in options"
+    :key="option.value"
+    :value="option.value"
+    :label="option.label"
+    :name="name"
+    :modelValue="modelValue"
+    @update:modelValue="$emit('update:modelValue', $event)"
+  />
+</template>
+
+<script>
+import VRadio from "../VRadio/VRadio.vue";
+
+export default {
+  components: { VRadio },
+  emits: ["update:modelValue"],
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: [String, Number, Boolean],
+      required: true,
+    },
+  },
+};
+</script>
