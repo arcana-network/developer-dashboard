df367a67c7ef7fb38fd6090b677d36ea05c0cba1
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-04-22 06:59:21 +0530

    Replaced main.js with main.ts

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { createApp } from "vue";
+import VueGtag from "vue-gtag";
+import VWave from "v-wave";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import { sentry as sentryConfig } from "@/utils/constants";
+
+import "vue3-circle-progress/dist/circle-progress.css";
+import "@/components/lib/styles.css";
+
+function getDSN() {
+  if (import.meta.env.PROD) {
+    return sentryConfig.dsn;
+  }
+  sentryConfig.tracingOrigins = "";
+  return null;
+}
+
+const app = createApp(App);
+Sentry.init({
+  app,
+  dsn: getDSN(),
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: sentryConfig.tracingOrigins,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+app.use(router);
+app.use(store);
+app.use(VWave);
+
+if (import.meta.env.PROD) {
+  app.use(VueGtag, {
+    config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_ID },
+  });
+}
+
+app.mount("#app");
