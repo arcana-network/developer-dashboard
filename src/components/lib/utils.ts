df414f5af211bf59f604e2e38da94028f850d2e4
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-04-28 15:55:08 +0530

    Renamed component library utils js to utils ts

---
+++ src/components/lib/utils.ts
@@ -0,0 +1,30 @@
+const caches = {}
+
+export function getRandomId() {
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0]
+  return uint32.toString(16)
+}
+
+export function checkFlexGap() {
+  if (caches.isFlexGapSupported?.memoized) {
+    return caches.isFlexGapSupported.value
+  }
+  const flex = document.createElement('div')
+  flex.style.display = 'flex'
+  flex.style.flexDirection = 'column'
+  flex.style.rowGap = '1px'
+
+  flex.appendChild(document.createElement('div'))
+  flex.appendChild(document.createElement('div'))
+
+  document.body.appendChild(flex)
+  const isSupported = flex.scrollHeight === 1
+  flex.parentNode.removeChild(flex)
+
+  caches.isFlexGapSupported = {
+    memoized: true,
+    value: isSupported,
+  }
+
+  return isSupported
+}
