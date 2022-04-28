7a217f9b8afc3a91d03e91f3df9479645388c98c
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-04-28 16:01:07 +0530

    Adjusted stylings in VSeperator

--- src/components/lib/VSeperator/VSeperator.vue
+++ src/components/lib/VSeperator/VSeperator.vue
@@ -1,24 +1,34 @@
+<script lang="ts">
+const VSeperator = {
+  name: 'VSeperator',
+  props: {
+    vertical: Boolean,
+  },
+}
+
+export default VSeperator
+</script>
+
 <template>
   <hr v-if="!vertical" />
   <div v-else class="vr"></div>
 </template>
 
 <style scoped>
-hr,
 .vr {
-  border: 1px solid #000000;
-  box-shadow: 1px 1px 3px 4px rgba(45, 45, 45, 0.08),
-    inset -10px -10px 20px rgba(0, 0, 0, 0.5), inset 10px 10px 15px #222222;
-  filter: blur(1px);
+  width: 1px;
+  border: 1px solid #000;
   border-radius: 10px;
+  box-shadow: 1px 1px 3px 4px rgb(45 45 45 / 8%),
+    inset -10px -10px 20px rgb(0 0 0 / 50%), inset 10px 10px 15px #222;
+}
+
+hr {
+  width: 100%;
+  border: transparent;
+  border-top: 1px solid #000;
+  border-radius: 10px;
+  box-shadow: 1px 1px 3px 4px rgb(45 45 45 / 8%),
+    inset -10px -10px 20px rgb(0 0 0 / 50%), inset 10px 10px 15px #222;
 }
 </style>
-
-<script>
-export default {
-  props: {
-    vertical: Boolean,
-  },
-  name: "VSeperator",
-};
-</script>
