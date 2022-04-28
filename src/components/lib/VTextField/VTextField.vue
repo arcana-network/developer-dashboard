6764aa83bba9b15c7e03a1214c8507baddd70370
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-04-28 16:00:25 +0530

    Added new prop to VTextField

--- src/components/lib/VTextField/VTextField.vue
+++ src/components/lib/VTextField/VTextField.vue
@@ -1,133 +1,10 @@
-<template>
-  <div class="form-group" aria-label="Form group" :style="style">
-    <label v-if="label" :style="[labelStyle]" :for="componentId">
-      {{ label }}
-    </label>
-    <div class="text-field" :class="{ icon: !!icon }">
-      <input
-        :type="inputType"
-        :id="componentId"
-        :value="modelValue"
-        @input="onInput"
-        v-bind="attrs"
-        :placeholder="placeholder"
-        :class="{ strong }"
-        :style="inputStyle"
-      />
-      <img
-        v-if="icon"
-        :src="icon"
-        :class="{
-          'icon-clickable': clickableIcon,
-          height: '1.1em',
-        }"
-        @click.stop="onIconClicked"
-      />
-    </div>
-    <span class="message" :class="messageType" v-if="!noMessage">
-      {{ message || "Some message" }}
-    </span>
-  </div>
-</template>
+<script lang="ts">
+import { reactive, ref, computed } from 'vue'
 
-<style scoped>
-div.form-group {
-  display: inline-flex;
-  flex-direction: column;
-}
-label {
-  font-family: var(--font-body);
-  color: var(--text-grey);
-  font-weight: 400;
-  font-size: 1.125em;
-  line-height: 1.5em;
-  margin: 10px 20px;
-}
-div.text-field {
-  background: linear-gradient(141.48deg, #1a1a1a -4.56%, #151515 135.63%);
-  box-shadow: inset -2px -2px 4px rgba(57, 57, 57, 0.44),
-    inset 5px 5px 10px rgba(11, 11, 11, 0.5);
-  border-radius: 10px;
-  display: flex;
-}
-.message {
-  font-family: var(--font-body);
-  font-weight: 400;
-  visibility: hidden;
-  font-size: 0.94em;
-  line-height: 1.5em;
-  margin: 5px 20px;
-}
-.message.success {
-  visibility: visible;
-  color: #8fff00;
-}
-.message.error {
-  visibility: visible;
-  color: #ee193f;
-}
-.loading-state {
-  color: transparent;
-}
-input {
-  border: none;
-  box-shadow: none;
-  background: transparent;
-  outline: none;
-  --webkit-outline: none;
-  color: var(--text-white);
-  font-family: var(--font-body);
-  font-size: 1.1em;
-  line-height: 1.5em;
-  margin: 15px 20px;
-  padding: 0;
-  width: 100%;
-}
-div.text-field.icon input {
-  width: calc(100% - 80px);
-}
-input::placeholder,
-input::-webkit-input-placeholder,
-input::-moz-placeholder,
-input:-ms-input-placeholder,
-input:-moz-placeholder {
-  color: var(--text-grey);
-  font-family: var(--font-body);
-  font-size: 1.1em;
-  line-height: 1.5em;
-  margin: 10px 20px;
-}
-input.strong {
-  font-size: 1.4em;
-  line-height: 1.65em;
-  font-weight: 600;
-  margin: 5px 20px;
-}
-input:-webkit-autofill,
-input:-webkit-autofill:hover,
-input:-webkit-autofill:focus {
-  -webkit-box-shadow: 0 0 0px 1000px #181818 inset;
-  -webkit-text-fill-color: var(--text-white);
-}
-img.icon-clickable {
-  cursor: pointer;
-}
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-input[type="number"] {
-  -moz-appearance: textfield;
-}
-</style>
+import { getRandomId } from '@/components/lib/utils'
 
-<script>
-import { reactive, ref, computed } from "vue";
-import { getRandomId } from "../utils";
-
-export default {
-  name: "VTextField",
+const VTextField = {
+  name: 'VTextField',
   inheritAttrs: false,
   props: {
     labelStyle: [String, Object],
@@ -147,29 +24,30 @@ export default {
     inputStyle: [String, Object],
     noMessage: Boolean,
     style: [String, Object, Array],
+    inputContainerClass: String,
   },
   setup(props, { emit, attrs }) {
-    props = reactive(props);
-    let componentId = props.id?.trim() ?? getRandomId();
-    let iconClicked = ref(false);
+    props = reactive(props)
+    let componentId = props.id?.trim() ?? getRandomId()
+    let iconClicked = ref(false)
 
     let inputType = computed(() => {
-      return props.type?.trim().toLowerCase();
-    });
+      return props.type?.trim().toLowerCase()
+    })
 
     function validate(ev) {
-      if ("validation" in props && "call" in props.validation) {
-        props.validation.call(ev.target?.value);
+      if ('validation' in props && 'call' in props.validation) {
+        props.validation.call(ev.target?.value)
       }
     }
 
     function onInput(ev) {
-      emit("update:modelValue", ev.target?.value);
+      emit('update:modelValue', ev.target?.value)
     }
 
     function onIconClicked(ev) {
       if (props.clickableIcon) {
-        emit("icon-clicked", ev);
+        emit('icon-clicked', ev)
       }
     }
 
@@ -181,7 +59,151 @@ export default {
       inputType,
       onIconClicked,
       iconClicked,
-    };
+    }
   },
-};
+}
+
+export default VTextField
 </script>
+
+<template>
+  <div
+    class="form-group"
+    aria-label="Form group"
+    :style="style"
+    :class="inputContainerClass"
+  >
+    <label v-if="label" :style="[labelStyle]" :for="componentId">
+      {{ label }}
+    </label>
+    <div class="text-field" :class="{ icon: !!icon }">
+      <input
+        :id="componentId"
+        :type="inputType"
+        :value="modelValue"
+        v-bind="attrs"
+        :placeholder="placeholder"
+        :class="{ strong }"
+        :style="inputStyle"
+        @input="onInput"
+      />
+      <img
+        v-if="icon"
+        :src="icon"
+        :class="{
+          'icon-clickable': clickableIcon,
+          height: '1.1em',
+        }"
+        @click.stop="onIconClicked"
+      />
+    </div>
+    <span v-if="!noMessage" class="message" :class="messageType">
+      {{ message || 'Some message' }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+div.form-group {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+label {
+  margin: 10px 20px;
+  font-family: var(--font-body);
+  font-size: 1.125em;
+  font-weight: 400;
+  line-height: 1.5em;
+  color: var(--text-grey);
+}
+
+div.text-field {
+  display: flex;
+  background: linear-gradient(141.48deg, #1a1a1a -4.56%, #151515 135.63%);
+  border-radius: 10px;
+  box-shadow: inset -2px -2px 4px rgb(57 57 57 / 44%),
+    inset 5px 5px 10px rgb(11 11 11 / 50%);
+}
+
+.message {
+  margin: 5px 20px;
+  font-family: var(--font-body);
+  font-size: 0.94em;
+  font-weight: 400;
+  line-height: 1.5em;
+  visibility: hidden;
+}
+
+.message.success {
+  color: #8fff00;
+  visibility: visible;
+}
+
+.message.error {
+  color: #ee193f;
+  visibility: visible;
+}
+
+.loading-state {
+  color: transparent;
+}
+
+input {
+  width: 100%;
+  padding: 0;
+  margin: 15px 20px;
+  font-family: var(--font-body);
+  font-size: 1.1em;
+  line-height: 1.5em;
+  color: var(--text-white);
+  background: transparent;
+  border: none;
+  outline: none;
+  box-shadow: none;
+
+  --webkit-outline: none;
+}
+
+input::placeholder,
+input::placeholder,
+input:placeholder {
+  margin: 10px 20px;
+  font-family: var(--font-body);
+  font-size: 1.1em;
+  line-height: 1.5em;
+  color: var(--text-grey);
+}
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus {
+  box-shadow: 0 0 0 1000px #181818 inset;
+  -webkit-text-fill-color: var(--text-white);
+}
+
+img.icon-clickable {
+  cursor: pointer;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  appearance: none;
+  margin: 0;
+}
+
+div.text-field.icon input {
+  width: calc(100% - 80px);
+}
+
+input.strong {
+  margin: 5px 20px;
+  font-size: 1.4em;
+  font-weight: 600;
+  line-height: 1.65em;
+}
+
+input[type='number'] {
+  appearance: textfield;
+}
+</style>
