0279b77f55844f9b59171eeff798022ed4d676f6
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-05 11:20:20 +0530

    Added app controls section

---
+++ src/components/app-configure/general/AppControls.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import SettingCard from '@/components/app-configure/SettingCard.vue'
+import VButton from '@/components/lib/VButton/VButton.vue'
+</script>
+
+<template>
+  <section name="configure-app-controls">
+    <SettingCard>
+      <template #title>Controls</template>
+      <template #description
+        >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum
+        dolor sit amet, consectetuer adipiscing elit.</template
+      >
+      <VButton label="DELETE" variant="secondary" class="delete-button" />
+    </SettingCard>
+  </section>
+</template>
+
+<style scoped>
+.delete-button {
+  align-self: flex-start;
+}
+</style>
--- src/components/app-configure/general/GeneralSettings.vue
+++ src/components/app-configure/general/GeneralSettings.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
+import AppControls from '@/components/app-configure/general/AppControls.vue'
 import AppName from '@/components/app-configure/general/AppName.vue'
 import VStack from '@/components/lib/VStack/VStack.vue'
 </script>
 
 <template>
   <main>
-    <VStack direction="column">
+    <VStack direction="column" gap="2rem">
       <AppName />
+      <AppControls />
     </VStack>
   </main>
 </template>
