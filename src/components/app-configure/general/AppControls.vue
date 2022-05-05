c65f10d33a11685c9629cff10a09fe93061b97c5
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-05 12:58:40 +0530

    Added deletion logic

--- src/components/app-configure/general/AppControls.vue
+++ src/components/app-configure/general/AppControls.vue
@@ -1,6 +1,36 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import ConfirmDeletePopup from '@/components/app-configure/general/ConfirmDeletePopup.vue'
+import DeleteTimerPopup from '@/components/app-configure/general/DeleteTimerPopup.vue'
 import SettingCard from '@/components/app-configure/SettingCard.vue'
+import FullScreenLoader from '@/components/FullScreenLoader.vue'
 import VButton from '@/components/lib/VButton/VButton.vue'
+import VCard from '@/components/lib/VCard/VCard.vue'
+import VOverlay from '@/components/lib/VOverlay/VOverlay.vue'
+import appConfigService from '@/services/app-config.service'
+
+const router = useRouter()
+
+const deletePopup = ref(false)
+const deleteTimerPopup = ref(false)
+const isLoading = ref(false)
+const loadingMessage = ref('')
+
+function proceedDelete() {
+  deleteTimerPopup.value = true
+  deletePopup.value = false
+}
+
+async function deleteApp() {
+  deleteTimerPopup.value = false
+  isLoading.value = true
+  loadingMessage.value = 'Deleting App...'
+  await appConfigService.deleteApp()
+  isLoading.value = false
+  router.push('/')
+}
 </script>
 
 <template>
@@ -11,13 +41,46 @@ import VButton from '@/components/lib/VButton/VButton.vue'
         >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum
         dolor sit amet, consectetuer adipiscing elit.</template
       >
-      <VButton label="DELETE" variant="secondary" class="delete-button" />
+      <VButton
+        label="DELETE"
+        variant="secondary"
+        class="delete-button"
+        @click.stop="deletePopup = true"
+      />
     </SettingCard>
+    <VOverlay v-if="deletePopup || deleteTimerPopup">
+      <VCard class="modal-card">
+        <ConfirmDeletePopup
+          v-if="deletePopup"
+          @cancel="deletePopup = false"
+          @proceed="proceedDelete"
+        />
+        <DeleteTimerPopup
+          v-if="deleteTimerPopup"
+          @cancel="deleteTimerPopup = false"
+          @delete="deleteApp"
+        />
+      </VCard>
+    </VOverlay>
   </section>
+  <FullScreenLoader v-if="isLoading" :message="loadingMessage" />
 </template>
 
 <style scoped>
 .delete-button {
   align-self: flex-start;
+}
+
+.modal-card {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  flex-direction: column;
+  gap: 1vh;
+  width: 72%;
+  min-width: 200px;
+  max-width: 560px;
+  padding: 4em 2em;
+  transform: translate(-50%, -50%);
 }
 </style>
---
+++ src/components/app-configure/general/DeleteTimerPopup.vue
@@ -0,0 +1,119 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Vue3CircleProgress from 'vue3-circle-progress'
+
+import VButton from '@/components/lib/VButton/VButton.vue'
+
+const emit = defineEmits(['cancel', 'delete'])
+
+const timer = ref(59)
+let timerProgress = 59000
+const initialProgressPercentage = (timerProgress / 60000) * 100
+const timerProgressPercentage = ref(initialProgressPercentage)
+
+const timerInterval = setInterval(() => {
+  --timer.value
+  if (timer.value === 0) {
+    proceedDelete()
+  }
+}, 1000)
+
+const progressInterval = setInterval(() => {
+  timerProgress = timerProgress - 10
+  timerProgressPercentage.value = (timerProgress / 60000) * 100
+}, 10)
+
+function clearTimer() {
+  clearInterval(timerInterval)
+  clearInterval(progressInterval)
+}
+
+function handleCancelDelete() {
+  clearTimer()
+  emit('cancel')
+}
+
+function proceedDelete() {
+  clearTimer()
+  emit('delete')
+}
+</script>
+
+<template>
+  <header class="sub-heading-1 flex-grow">Deleting App...</header>
+  <div class="flex outer-clock">
+    <Vue3CircleProgress
+      :percent="timerProgressPercentage"
+      :border-width="5"
+      :border-bg-width="5"
+      fill-color="#13A3FD"
+      empty-color="transparent"
+      :size="200"
+    />
+    <div class="flex inner-clock">
+      <span class="sub-heading-2">
+        00:{{ timer.toString().padStart(2, '0') }}
+      </span>
+    </div>
+  </div>
+  <footer class="flex sm-column flex-grow">
+    <VButton
+      v-wave
+      variant="secondary"
+      label="CANCEL"
+      @click.stop="handleCancelDelete"
+    />
+    <VButton
+      v-wave
+      variant="primary"
+      label="CONFIRM"
+      @click.stop="proceedDelete"
+    />
+  </footer>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+}
+
+footer {
+  gap: 2vh;
+  align-items: center;
+  justify-content: center;
+}
+
+.outer-clock {
+  position: relative;
+  align-items: center;
+  align-self: center;
+  justify-content: center;
+  width: 200px;
+  height: 200px;
+  margin: 3em 0;
+  background: #000;
+  border-radius: 50%;
+  box-shadow: inset 0 -13px 33px -10px rgb(255 255 255 / 15%),
+    inset 0 10px 53px 72px #090909;
+}
+
+.inner-clock {
+  position: absolute;
+  z-index: 1;
+  align-items: center;
+  justify-content: center;
+  width: 94%;
+  height: 94%;
+  background: linear-gradient(
+    45.51deg,
+    #000 -78.45%,
+    rgb(31 31 31 / 29%) 88.21%
+  );
+  border-radius: 50%;
+  box-shadow: 5px 5px 10px rgb(0 0 0 / 18%),
+    10px 10px 17px -4px rgb(10 10 10 / 50%),
+    -8px -8px 20px -2px rgb(255 255 255 / 6%),
+    inset 5px 5px 30px 8px rgb(32 32 32 / 6%);
+}
+</style>
