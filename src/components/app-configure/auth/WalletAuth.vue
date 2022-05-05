18bbe7644465e2181c03d426a45a217d5bc4c98b
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-05 14:04:35 +0530

    Added wallet auth settings

---
+++ src/components/app-configure/auth/WalletAuth.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import SettingCard from '@/components/app-configure/SettingCard.vue'
+</script>
+
+<template>
+  <section name="configure-wallet-auth">
+    <SettingCard>
+      <template #title>Wallet Auth</template>
+      <template #description
+        >Allow your users to authenticate themselves by signing transactions
+        using wallets. <a>LEARN MORE</a></template
+      >
+    </SettingCard>
+  </section>
+</template>
