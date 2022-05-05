615e5b05fadb69f4de384b368affbb6b98f412a9
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-05 16:04:06 +0530

    Added wallet auth settings (#76)
    * Added wallet auth settings
    
    * renamed size to font-size
    
    * removed link class from style guide

---
+++ src/components/app-configure/auth/AuthSettings.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import WalletAuth from '@/components/app-configure/auth/WalletAuth.vue'
+import VStack from '@/components/lib/VStack/VStack.vue'
+</script>
+
+<template>
+  <main>
+    <VStack direction="column">
+      <WalletAuth />
+    </VStack>
+  </main>
+</template>
