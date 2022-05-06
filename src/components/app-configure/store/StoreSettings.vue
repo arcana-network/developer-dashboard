8ce65acb447480024ccfac1a2757215f543d9b41
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-06 13:42:48 +0530

    Added asset encryption section (#79)

---
+++ src/components/app-configure/store/StoreSettings.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import AssetEncryption from '@/components/app-configure/store/AssetEncryption.vue'
+import VStack from '@/components/lib/VStack/VStack.vue'
+</script>
+
+<template>
+  <main>
+    <VStack direction="column" gap="2rem">
+      <AssetEncryption />
+    </VStack>
+  </main>
+</template>
