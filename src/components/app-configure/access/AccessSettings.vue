d4cfd1ca774f37cfa7bfd04555ceaf6154f53360
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-05-12 18:29:01 +0530

    Removed UserAccess and AssetEncryption sections

--- src/components/app-configure/access/AccessSettings.vue
+++ src/components/app-configure/access/AccessSettings.vue
@@ -1,13 +1,11 @@
 <script lang="ts" setup>
 import ChainType from '@/components/app-configure/access/ChainType.vue'
-import UserAccess from '@/components/app-configure/access/UserAccess.vue'
 import VStack from '@/components/lib/VStack/VStack.vue'
 </script>
 
 <template>
   <main>
     <VStack direction="column" gap="2rem">
-      <UserAccess />
       <ChainType />
     </VStack>
   </main>
--- src/components/app-configure/store/StoreSettings.vue
+++ src/components/app-configure/store/StoreSettings.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import AssetEncryption from '@/components/app-configure/store/AssetEncryption.vue'
 import StorageRegion from '@/components/app-configure/store/StorageRegion.vue'
 import UserLimits from '@/components/app-configure/store/UserLimits.vue'
 import VStack from '@/components/lib/VStack/VStack.vue'
@@ -8,7 +7,6 @@ import VStack from '@/components/lib/VStack/VStack.vue'
 <template>
   <main>
     <VStack direction="column" gap="2rem">
-      <AssetEncryption />
       <StorageRegion />
       <UserLimits />
     </VStack>
