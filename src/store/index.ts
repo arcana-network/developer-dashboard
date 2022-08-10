645dfee6f2a3414df334a4581bacefff4e0c7c63
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-08-10 17:01:18 +0530

    Remove loader from vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,27 +2,20 @@ import { createStore, createLogger } from 'vuex'
 
 import appStore from '@/store/app.store'
 import authStore from '@/store/auth.store'
-import toastStore from '@/store/toast.store'
 
 const debug = process.env.NODE_ENV !== 'production'
 
 type State = {
-  isLoading: boolean
-  loadingMessage: string
   forwarder: string
   rpcUrl: string
 }
 
 const state: State = {
-  isLoading: false,
-  loadingMessage: '',
   forwarder: '',
   rpcUrl: '',
 }
 
 const getters = {
-  isLoading: (state: State) => state.isLoading,
-  loadingMessage: (state: State) => state.loadingMessage,
   forwarder: (state: State) => state.forwarder,
   rpcUrl: (state: State) => state.rpcUrl,
 }
@@ -31,13 +24,6 @@ const mutations = {
   resetStore(state: State) {
     state.forwarder = ''
     state.rpcUrl = ''
-  },
-  showLoader(state: State, loadingMessage: string) {
-    state.isLoading = true
-    state.loadingMessage = loadingMessage
-  },
-  hideLoader(state: State) {
-    state.isLoading = false
   },
   updateForwarder(state: State, forwarder: string) {
     state.forwarder = forwarder
@@ -57,7 +43,6 @@ const store = createStore({
   modules: {
     appStore,
     authStore,
-    toastStore,
   },
   state,
   mutations,
