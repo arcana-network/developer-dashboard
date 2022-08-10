5aedd11e202036aac1ae7d48215563bb9293dd70
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-08-10 17:47:56 +0530

    Remove vuex auth store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,13 @@
 import { createStore, createLogger } from 'vuex'
 
 import appStore from '@/store/app.store'
-import authStore from '@/store/auth.store'
 
 const debug = process.env.NODE_ENV !== 'production'
-
-const actions = {
-  resetStore({ commit }) {
-    commit('resetStore')
-  },
-}
 
 const store = createStore({
   modules: {
     appStore,
-    authStore,
   },
-  actions,
   strict: debug,
   plugins: debug ? [createLogger()] : [],
 })
