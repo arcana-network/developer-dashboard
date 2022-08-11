f4c42b36050b13d9928f3ec362b10d5dfaadc9bd
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-08-11 14:07:52 +0530

    Add types for env

--- env.d.ts
+++ env.d.ts
@@ -7,18 +7,38 @@ declare module '*.vue' {
   export default component
 }
 declare global {
+  import type { ArcanaProvider } from '@arcana/auth'
+
   type SmartContractAcceptedValue = (string | undefined | number)[]
 
   type TransactionSignerParams = {
     appAddress: string
-    provider: any
+    provider: ArcanaProvider
     forwarderAddress: string
     gateway: string
     accessToken: string
   }
 
+  interface ImportMetaEnv {
+    PROD: boolean
+    VITE_ARCANA_APP_ID: string
+    VITE_GATEWAY_API: string
+    VITE_VERIFY_URI: string
+    VITE_DOCS_URL: string
+    VITE_CURRENT_API_VERSION: string
+    VITE_ARCANA_AUTH_NETWORK: 'dev' | 'testnet'
+    VITE_GOOGLE_ANALYTICS_ID?: string
+    VITE_IS_APP_DOWN?: boolean
+    VITE_MAILCHIMP_DEV_PORTAL_ID?: string
+    VITE_MAILCHIMP_GROUP?: string
+    VITE_MAILCHIMP_LIST_ID?: string
+    VITE_MAILCHIMP_LIST_URL?: string
+    VITE_MAILCHIMP_USER_ID?: string
+    VITE_SENTRY_DSN: string
+    VITE_SENTRY_TRACING_ORIGINS: string
+  }
   interface ImportMeta {
-    readonly env: any
+    readonly env: ImportMetaEnv
   }
 
   interface Window {
