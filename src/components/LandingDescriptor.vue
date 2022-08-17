8f1c76744f4bcc7b331c8b57a1f7d744bfc1ffac
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-08-17 14:03:14 +0530

    Move inline styles to css and update height of separator

--- src/components/LandingDescriptor.vue
+++ src/components/LandingDescriptor.vue
@@ -4,30 +4,31 @@ import VSeperator from '@/components/lib/VSeperator/VSeperator.vue'
 </script>
 
 <template>
-  <div
-    class="mobile-remove tablet-remove"
-    style="
-      width: 40%;
-      min-width: 400px;
-      max-width: 488px;
-      margin-top: 8vh;
-      margin-left: 4vw;
-    "
-  >
+  <div class="mobile-remove tablet-remove landing-descriptor">
     <img :src="AppLogo" style="height: 72px" alt="Arcana Logo" />
     <h1 style="margin-top: 4vh; margin-right: 4vw">
       Make your dApps secure from the ground up!
     </h1>
     <img src="@/assets/sidebar-illustration.png" style="margin: 6vh auto 0" />
   </div>
-  <v-seperator
-    :vertical="true"
-    style="position: sticky; top: 0; height: 99.4vh"
-    class="mobile-remove tablet-remove"
-  />
+  <VSeperator :vertical="true" class="mobile-remove tablet-remove separator" />
 </template>
 
 <style scoped>
+.landing-descriptor {
+  width: 40%;
+  min-width: 400px;
+  max-width: 488px;
+  margin-top: 8vh;
+  margin-left: 4vw;
+}
+
+.separator {
+  position: sticky;
+  top: 0;
+  height: calc(100vh - 5rem);
+}
+
 h1 {
   font-weight: 700;
   letter-spacing: unset;
