53de10d82c526de60d8a1cebbaf7f668630621f5
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-09-06 17:20:20 +0530

    Adjust styling of configure footer

--- src/components/app-configure/ConfigureFooter.vue
+++ src/components/app-configure/ConfigureFooter.vue
@@ -16,59 +16,36 @@ const emit = defineEmits(['cancel', 'save'])
 </script>
 
 <template>
-  <footer>
-    <div class="container flex flex-wrap">
-      <v-button
-        v-wave
-        :label="cancelLabel"
-        variant="secondary"
-        @click.stop="emit('cancel')"
-      />
-      <v-button
-        v-wave
-        :label="saveLabel"
-        variant="primary"
-        @click.stop="emit('save')"
-      />
-    </div>
-  </footer>
+  <div class="footer flex flex-wrap">
+    <VButton
+      v-wave
+      :label="cancelLabel"
+      variant="secondary"
+      @click.stop="emit('cancel')"
+    />
+    <VButton
+      v-wave
+      type="submit"
+      :label="saveLabel"
+      variant="primary"
+      @click.stop="emit('save')"
+    />
+  </div>
 </template>
 
 <style scoped>
-footer {
-  position: sticky;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 100;
+.footer {
   display: flex;
-  padding: 1.25rem 0;
-  background: var(--dark-theme-bg);
-}
-
-footer div {
   gap: 2rem;
   justify-content: flex-end;
-}
-
-footer.show {
-  visibility: visible;
-  opacity: 1;
+  margin-top: 1.25rem;
 }
 
 @media only screen and (max-width: 767px) {
-  footer {
-    padding-bottom: 0.5rem;
-  }
-
-  footer div {
+  .footer {
     flex-wrap: wrap-reverse;
     gap: 0.5rem;
     justify-content: center;
-  }
-
-  .container {
-    margin-bottom: 1vh;
   }
 }
 </style>
