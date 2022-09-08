240a51b3b0f3ab2dcd0b9a5e0d1b34ab277d08db
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-09-08 17:08:06 +0530

    Remove comments

--- src/pages/AppConfigure.vue
+++ src/pages/AppConfigure.vue
@@ -18,9 +18,6 @@ currentTab.value = String(route.name)
   .replace('Settings', '')
   .toLowerCase() as ConfigureTabType
 
-// const currentAppState = appsStore.app(appId)
-// let currentConfig = getAppConfigRequestBody(currentAppState)
-
 function switchTab(tab: ConfigureTab) {
   currentTab.value = tab.type
   router.push({
@@ -28,71 +25,6 @@ function switchTab(tab: ConfigureTab) {
     params: { appId },
   })
 }
-
-// async function handleSave() {
-//   loaderStore.showLoader('Saving app config...')
-//   const appConfigRequestBody = getAppConfigRequestBody(appId)
-//   const updatedApp = (await updateApp(appId, appConfigRequestBody)).data
-//   const currentApp = appsStore.app(appId)
-//   currentApp.auth.wallet.walletTypeInGateway = updatedApp.app.wallet_type
-//   appsStore.updateApp(appId, currentApp)
-//   await updateSmartContractTransactions(appConfigRequestBody)
-//   currentConfig = appConfigRequestBody
-//   loaderStore.hideLoader()
-//   toast.success('App configuration updated')
-// }
-
-// async function updateSmartContractTransactions(app: AppConfigRequiredProps) {
-//   try {
-//     const hasStorageLimitChanged =
-//       app.storage_limit !== currentConfig.storage_limit
-//     const hasBandwidthLimitChanged =
-//       app.bandwidth_limit !== currentConfig.bandwidth_limit
-
-//     if (hasStorageLimitChanged || hasBandwidthLimitChanged) {
-//       loaderStore.showLoader('Updating user limits in smart contract...')
-//       await setDefaultLimit(app.storage_limit, app.bandwidth_limit)
-//       toast.success('User limits saved in blockchain')
-//     }
-//   } catch (e) {
-//     handleSmartContractErrors('User limits', e)
-//   }
-
-//   try {
-//     if (
-//       app.wallet_type === WalletMode.UI &&
-//       currentConfig.wallet_type === WalletMode.NoUI
-//     ) {
-//       loaderStore.showLoader('Enabling UI Mode in smart contract...')
-//       await enableUiMode()
-//       toast.success('UI mode saved in blockchain')
-//     }
-//   } catch (e) {
-//     handleSmartContractErrors('UI mode', e)
-//   }
-
-//   try {
-//     loaderStore.showLoader('Updating app config in smart contract...')
-//     await setAppConfig(app.name as string, app.cred as AppConfigCred[])
-//     toast.success('App config saved in blockchain')
-//   } catch (e) {
-//     handleSmartContractErrors('App Config', e)
-//   }
-
-//   loaderStore.hideLoader()
-// }
-
-// function handleSmartContractErrors(type: string, error: unknown) {
-//   toast.error(
-//     `An error occurred while saving the ${type} in the blockchain. Please try again or contact support.`
-//   )
-//   console.error(error)
-// }
-
-// function handleCancel() {
-//   appsStore.fetchAndStoreAppConfig(appId)
-//   router.back()
-// }
 </script>
 
 <template>
@@ -103,11 +35,6 @@ function switchTab(tab: ConfigureTab) {
       <ConfigureSidebar :current-tab="currentTab" @switch-tab="switchTab" />
       <RouterView />
     </VStack>
-    <!-- <ConfigureFooter
-      class="configure-footer"
-      @save="handleSave"
-      @cancel="handleCancel"
-    /> -->
   </VStack>
 </template>
 
