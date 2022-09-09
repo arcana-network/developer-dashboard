466f67b2eeeab800e196dcff4f6243e9a258af19
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2022-09-09 14:28:38 +0530

    Add form to create app

--- src/components/app-configure/CreateApp.vue
+++ src/components/app-configure/CreateApp.vue
@@ -105,44 +105,46 @@ async function handleCreateApp() {
     <VCard variant="popup" class="create-app-modal-card">
       <h2 class="create-app-title">Create New App</h2>
       <VSeperator />
-      <VStack direction="column" gap="1rem">
-        <label class="app-name-label">Enter App Name</label>
-        <VTextField
-          v-model="appName"
-          class="app-name-input"
-          :message-type="hasAppNameError ? 'error' : ''"
-          message="App Name cannot be empty"
-        />
-      </VStack>
-      <VStack direction="column" gap="1rem" align="start">
-        <VStack gap="0.5rem">
-          <label class="app-name-label">Choose Region</label>
-          <VTooltip
-            title="Arcana Store uses physical storage nodes that are logically grouped by
+      <form @submit.prevent="handleCreateApp">
+        <VStack direction="column" gap="1rem">
+          <label class="app-name-label">Enter App Name</label>
+          <VTextField
+            v-model.trim="appName"
+            class="app-name-input"
+            :message-type="hasAppNameError ? 'error' : ''"
+            message="App Name cannot be empty"
+          />
+        </VStack>
+        <VStack direction="column" gap="1rem" align="start">
+          <VStack gap="0.5rem">
+            <label class="app-name-label">Choose Region</label>
+            <VTooltip
+              title="Arcana Store uses physical storage nodes that are logically grouped by
         geography. This allows you to control the region or location where
         dApp's data assets reside, for compliance and regulatory purpose. <strong>Once a
         region has been selected it cannot be altered.</strong>"
-          >
-            <img
-              src="@/assets/iconography/info-circle-outline.svg"
-              style="cursor: pointer"
-            />
-          </VTooltip>
+            >
+              <img
+                src="@/assets/iconography/info-circle-outline.svg"
+                style="cursor: pointer"
+              />
+            </VTooltip>
+          </VStack>
+          <VDropdown
+            :options="regions"
+            display-field="name"
+            class="region-dropdown"
+            :model-value="selectedRegion"
+            @update:model-value="handleRegionChange"
+          />
         </VStack>
-        <VDropdown
-          :options="regions"
-          display-field="name"
-          class="region-dropdown"
-          :model-value="selectedRegion"
-          @update:model-value="handleRegionChange"
+        <VButton
+          type="submit"
+          label="CREATE"
+          class="create-button"
+          :disabled="!appName?.trim()"
         />
-      </VStack>
-      <VButton
-        label="CREATE"
-        class="create-button"
-        :disabled="!appName?.trim()"
-        @click.stop="handleCreateApp"
-      />
+      </form>
     </VCard>
   </VOverlay>
 </template>
