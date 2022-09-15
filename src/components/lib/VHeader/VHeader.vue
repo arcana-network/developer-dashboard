ebfe7474511b495cfe6f8197e5f9653121d2f23a
Ajith Ranka <11095038+ajithranka@users.noreply.github.com>
2022-09-15 06:56:25 +0100

    Add temporary navigation fixes on dev dashboard (#131)
    * Add apps link and disable dashboard, users links
    
    * Separte rules for disabling dashboard and users pages
    
    Co-authored-by: Ajith Ranka <ajithranka@users.noreply.github.com>

--- src/components/lib/VHeader/VHeader.vue
+++ src/components/lib/VHeader/VHeader.vue
@@ -65,6 +65,7 @@ const logoAltText = computed(() => {
 const showMenu = ref(false)
 
 function handleMenuItemClick(menuItem) {
+  if (menuItem.disabled) return
   if (menuItem.action) {
     menuItem.action.call()
   }
@@ -137,11 +138,11 @@ function onLogoClick() {
             }"
             role="listitem"
             class="menu-item"
-            :class="
-              selectedItem === 'menu-item' && menuItem.selected
-                ? 'menu-item-selected'
-                : ''
-            "
+            :class="{
+              'menu-item-selected':
+                selectedItem === 'menu-item' && menuItem.selected,
+              'menu-item-disabled': menuItem.disabled,
+            }"
             @click.stop="handleMenuItemClick(menuItem)"
           >
             {{ menuItem.label }}
@@ -316,6 +317,11 @@ header section {
 
 .menu-item-selected {
   color: #13a3fd;
+}
+
+.menu-item-disabled {
+  pointer-events: none;
+  opacity: 0.25;
 }
 
 @media only screen and (max-width: 1023px) {
