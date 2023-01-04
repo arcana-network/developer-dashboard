d35a02998acd8c0b220c31ae7ac2f02fe2af1d28
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-04 19:14:17 +0530

    comment out footer for now

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ onBeforeMount(async () => {
         <component :is="Component" />
       </transition>
     </router-view>
-    <AppFooter v-if="isAuthLoaded && !route.path.includes('/config/')" />
+    <!-- <AppFooter v-if="isAuthLoaded && !route.path.includes('/config/')" /> -->
     <FullScreenLoader
       v-if="loaderStore.isLoading || !isAuthLoaded"
       :message="loaderStore.message"
