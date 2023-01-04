ea25c9ef44c9d08fa2540a1b2f9cb31c8d2c301f
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-04 19:12:23 +0530

    add sidebar to app details component

--- src/pages/AppDetails.vue
+++ src/pages/AppDetails.vue
@@ -1,13 +1,28 @@
 <script lang="ts" setup>
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, ref, type Ref } from 'vue'
+import { useRouter } from 'vue-router'
 
+import ConfigureSidebar from '@/components/app-configure/ConfigureSidebar.vue'
+import VStack from '@/components/lib/VStack/VStack.vue'
 import { useAppsStore } from '@/stores/apps.store'
 import { useLoaderStore } from '@/stores/loader.store'
 import { useAppId } from '@/use/getAppId'
+import type { ConfigureTab, ConfigureTabType } from '@/utils/constants'
 import { createTransactionSigner } from '@/utils/signerUtils'
 
 const appsStore = useAppsStore()
 const loaderStore = useLoaderStore()
+const currentTab: Ref<ConfigureTabType> = ref('dashboard')
+const router = useRouter()
+const appId = useAppId()
+
+function switchTab(tab: ConfigureTab) {
+  currentTab.value = tab.type
+  router.push({
+    name: `${tab.label}Settings`,
+    params: { appId },
+  })
+}
 
 onBeforeMount(async () => {
   const appId = useAppId()
@@ -20,5 +35,44 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <RouterView />
+  <VStack direction="row" class="app-details__container">
+    <div class="app-details__sidebar">
+      <ConfigureSidebar :current-tab="currentTab" @switch-tab="switchTab" />
+    </div>
+    <VStack direction="column" class="app-details__content">
+      <div class="flex justify-end">
+        <button class="help-button">Help</button>
+      </div>
+      <RouterView />
+    </VStack>
+  </VStack>
 </template>
+
+<style scoped>
+.app-details__container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  overflow: hidden;
+}
+
+.app-details__content {
+  box-sizing: border-box;
+  flex: 1;
+  height: 100%;
+  padding-top: 2rem;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+.help-button {
+  margin-right: 4rem;
+  font-family: var(--font-body);
+  color: var(--primary);
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  outline: none;
+}
+</style>
