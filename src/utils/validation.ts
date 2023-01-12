ae7c63e635869096a7d30582c0cc6ce594dffe17
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-01-12 08:53:55 +0530

    Update email validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -16,17 +16,26 @@ const URLRegex = new RegExp(
 )
 
 function isValidEmail(email: string) {
-  const splitEmails = email.split('@')
-  let decodedEmail = ''
-  if (splitEmails[0]) {
-    decodedEmail += toASCII(splitEmails[0])
+  if (email.includes('@')) {
+    const splitEmails = email.split('@')
+    let decodedEmail = ''
+    if (
+      splitEmails[0].startsWith('.') ||
+      splitEmails[0].endsWith('.') ||
+      splitEmails[0].length === 0
+    ) {
+      return false
+    }
+    if (splitEmails[1].length > 0) {
+      decodedEmail = `@${toASCII(splitEmails[1])}`
+    } else {
+      return false
+    }
+    return /^(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([\w]{2,24}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i.test(
+      decodedEmail
+    )
   }
-  if (splitEmails[1]) {
-    decodedEmail += '@' + toASCII(splitEmails[1])
-  }
-  return /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([\w]{2,24}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i.test(
-    decodedEmail
-  )
+  return false
 }
 
 function isValidUrl(url: string) {
