3a1d84e544d5eb48b1370ad393f48a69e60f246b
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-21 13:31:52 +0530

    access gateway URL through network prop

--- src/utils/get-env-api.ts
+++ src/utils/get-env-api.ts
@@ -1,12 +1,13 @@
-import constants from '@/utils/constants'
+import constants, { type Network } from '@/utils/constants'
 
 const CURRENT_API_VERSION =
   import.meta.env.VITE_ARCANA_GATEWAY_API_VERSION || ''
 
 export default function getEnvApi(
-  version: string | boolean = CURRENT_API_VERSION
+  version: string | boolean = CURRENT_API_VERSION,
+  network: Network = 'testnet'
 ): string {
-  let apiEndpoint = constants.api.gateway
+  let apiEndpoint = constants.api.gateway[network]
 
   if (version === false) {
     return apiEndpoint
