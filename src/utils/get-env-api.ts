898d6922763cf4401435394840c2c1ccd39cd8cb
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-21 18:02:20 +0530

    remove code that attaches baseURL

--- src/utils/get-env-api.ts
+++ src/utils/get-env-api.ts
@@ -1,24 +1,10 @@
-import constants, { type Network } from '@/utils/constants'
-
 const CURRENT_API_VERSION =
   import.meta.env.VITE_ARCANA_GATEWAY_API_VERSION || ''
 
 export default function getEnvApi(
-  version: string | boolean = CURRENT_API_VERSION,
-  network: Network = 'testnet'
+  version: string | boolean = CURRENT_API_VERSION
 ): string {
-  let apiEndpoint = constants.api.gateway[network]
-
-  if (version === false) {
-    return apiEndpoint
-  }
-
   if (version) {
-    version = '/' + version
-  }
-
-  if (!apiEndpoint.endsWith('/')) {
-    apiEndpoint += '/'
-  }
-  return apiEndpoint + 'api' + version
+    return `/api/${version}`
+  } else return ''
 }
