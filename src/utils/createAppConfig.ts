c6924cdc0412bb6113a15b5ed9ae3911d2f0164f
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-23 12:36:08 +0530

    create util to construct app config

---
+++ src/utils/createAppConfig.ts
@@ -0,0 +1,50 @@
+import type { AppConfig as ApiAppResponse } from '@/services/gateway.service'
+import { getThemeLogo } from '@/services/gateway.service'
+import type { AppConfig } from '@/stores/apps.store'
+import type { Network } from '@/utils/constants'
+import { ChainMapping, api, type Chain } from '@/utils/constants'
+
+export function createAppConfig(
+  app: ApiAppResponse,
+  network: Network
+): AppConfig {
+  return {
+    id: app.ID,
+    name: app.name as string,
+    address: app.address as string,
+    totalUsers: 0,
+    region: 'asia',
+    network,
+    logos: {
+      dark: {
+        horizontal: app.logo?.dark_horizontal
+          ? getThemeLogo(app.ID, 'dark', 'horizontal').url
+          : '',
+        vertical: app.logo?.dark_vertical
+          ? getThemeLogo(app.ID, 'dark', 'vertical').url
+          : '',
+      },
+      light: {
+        horizontal: app.logo?.light_horizontal
+          ? getThemeLogo(app.ID, 'light', 'horizontal').url
+          : '',
+        vertical: app.logo?.light_vertical
+          ? getThemeLogo(app.ID, 'light', 'vertical').url
+          : '',
+      },
+    },
+    access: {
+      selectedChain: app.chain ? (ChainMapping[app.chain] as Chain) : 'none',
+    },
+    auth: {
+      social: [],
+      wallet: {
+        walletType: app.wallet_type,
+        walletTypeInGateway: app.wallet_type,
+        websiteDomain: app.wallet_domain,
+        selectedTheme: app.theme || 'dark',
+      },
+      redirectUri: `${api.verify[network]}/${app.ID}/`,
+    },
+  }
+}
