c2d4b1927bdfb8c691c8c36e2b797e9ace26cc97
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-01-23 12:45:44 +0530

    access API url based on network

--- src/utils/signerUtils.ts
+++ src/utils/signerUtils.ts
@@ -1,6 +1,7 @@
 import { getConfig } from '@/services/gateway.service'
 import { useAuthStore } from '@/stores/auth.store'
 import type { Network } from '@/utils/constants'
+import { api } from '@/utils/constants'
 import getEnvApi from '@/utils/get-env-api'
 
 const authStore = useAuthStore()
@@ -31,7 +32,8 @@ let signTransaction: (
 ) => Promise<string>
 
 function getTransactionRequestProps(
-  appAddress: string
+  appAddress: string,
+  network: Network
 ): SmartContractRequestParams {
   if (!appAddress.startsWith('0x')) {
     appAddress = `0x${appAddress}`
@@ -43,13 +45,13 @@ function getTransactionRequestProps(
     appAddress,
     gateway: getEnvApi(),
     forwarderAddress: config.forwarder,
-    accessToken: authStore.accessToken,
+    accessToken: authStore.accessToken[network],
   }
 }
 
-async function createTransactionSigner(address: string) {
+async function createTransactionSigner(address: string, network: Network) {
   const { appAddress, gateway, forwarderAddress, accessToken } =
-    getTransactionRequestProps(address)
+    getTransactionRequestProps(address, network)
   const provider = window.arcana.provider
   signTransaction = window.transactionSigner.createTransactionSigner({
     appAddress,
