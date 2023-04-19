315a4bb8480d2152b5ca13088d9643393ec5f9aa
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-04-19 16:43:11 +0530

    create UI for search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SearchIcon from '@/assets/iconography/search.svg'
+
+console.log('Search bar')
+</script>
+
+<template>
+  <div
+    class="flex space-x-3 bg-[#313131] items-center w-[200px] p-2 rounded-md"
+  >
+    <img :src="SearchIcon" alt="search" class="h-[18px] w-[18px]" />
+    <input
+      type="text"
+      class="bg-transparent flex-1"
+      placeholder="Search a Chain"
+    />
+  </div>
+</template>
