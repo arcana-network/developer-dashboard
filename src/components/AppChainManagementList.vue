03a8e49d2dfbfd18c5ac94622896dd6ce0b7fca5
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-04-21 16:58:28 +0530

    implement code to
    1. Show fetched chains in the table.
    2. Replace some css with tailwind classes.
    3. Loigic to enable editing of chain.

--- src/components/AppChainManagementList.vue
+++ src/components/AppChainManagementList.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, toRefs } from 'vue'
 
-import ChainIcon from '@/assets/iconography/github.svg'
 import MoreIcon from '@/assets/iconography/more.svg'
 import VSwitchVue from '@/components/lib/VSwitch/VSwitch.vue'
+import { useChainManagementStore } from '@/stores/chainManagement.store'
 import { useClickOutside } from '@/use/clickOutside'
 
-const enable = ref(false)
-const showRowOptions = ref(false)
+const emits = defineEmits(['edit'])
+
+const chainManagementStore = useChainManagementStore()
+const { chains } = toRefs(chainManagementStore)
+const showRowOptionsOf = ref(null)
 const showRowOptions_menu = ref(null)
 
 const rowOptions = [
@@ -26,61 +29,64 @@ const rowOptions = [
 ]
 
 useClickOutside(showRowOptions_menu, () => {
-  showRowOptions.value = false
+  showRowOptionsOf.value = null
 })
+
+function onClickOfOption(option: string, chainId: string) {
+  if (option === 'Edit') emits('edit', { chainId })
+  showRowOptionsOf.value = null
+}
 </script>
 
 <template>
-  <div class="table-container | rounded-md">
+  <div class="table-container | rounded-md border-[1px] border-[#363636]">
     <table
       class="table-fixed text-white overflow-x-auto border-collapse w-full"
     >
-      <thead>
-        <tr>
-          <th class="w-[5%]"></th>
+      <thead class="border-b-[1px] border-b-[#363636]">
+        <tr class="text-[#8d8d8d]">
           <th class="w-[15%]">Name</th>
-          <th>Network</th>
-          <th>ID</th>
-          <th>Symbol</th>
-          <th>Compatibility</th>
-          <th>Type</th>
+          <th class="w-[5%]">ID</th>
+          <th class="w-[10%]">Symbol</th>
+          <th class="w-[10%]">Compatibility</th>
+          <th class="w-[10%]">Type</th>
           <th>RPC URL</th>
-          <th>Enabled</th>
+          <th class="w-[10%]">Enabled</th>
           <th class="w-[5%]"></th>
         </tr>
       </thead>
       <tbody>
-        <tr class="hover:bg-[#363636]">
-          <td>
-            <img :src="ChainIcon" alt="chain icon" class="w-6 h-6" />
-          </td>
-          <td class="text-ellipsis">Polygon Mainnet</td>
-          <td class="text-ellipsis">Polygon POS</td>
-          <td class="text-ellipsis">137</td>
-          <td class="text-ellipsis">MATIC</td>
-          <td class="text-ellipsis">EVM</td>
-          <td class="text-ellipsis">Mainnet</td>
-          <td class="text-ellipsis">https://polygon-rpc.com</td>
-          <td><VSwitchVue v-model:model-value="enable" /></td>
+        <tr
+          v-for="chain in chains"
+          :key="chain.chain_id"
+          class="hover:bg-[#363636]"
+        >
+          <td class="text-ellipsis">{{ chain.name }}</td>
+          <td class="text-ellipsis">{{ chain.id }}</td>
+          <td class="text-ellipsis">{{ chain.currency }}</td>
+          <td class="text-ellipsis">{{ chain.compatibility }}</td>
+          <td class="text-ellipsis">{{ chain.chain_type }}</td>
+          <td class="text-ellipsis">{{ chain.rpc_url }}</td>
+          <td><VSwitchVue :value="chain.status === 'true'" /></td>
           <td>
             <div class="relative">
               <button
                 class="flex justify-center items-center cursor-pointer w-7 h-7 bg-[#262626] rounded-[5px]"
-                @click.stop="showRowOptions = !showRowOptions"
+                @click.stop="showRowOptionsOf = chain.id"
               >
                 <img :src="MoreIcon" alt="more" />
               </button>
               <dialog
-                v-if="showRowOptions"
+                v-if="chain.id === showRowOptionsOf"
                 ref="showRowOptions_menu"
                 open
-                class="flex flex-col bg-[#1F1F1F] text-[#FFFFFF] rounded-md border-[1px] border-[#363636] p-2 space-y-1 absolute w-32 left-[-100px] top-[10px]"
+                class="flex flex-col bg-[#1F1F1F] text-[#FFFFFF] rounded-md border-[1px] border-[#363636] p-2 space-y-1 absolute w-32 left-[-100px] top-[10px] z-[999]"
               >
                 <button
                   v-for="option in rowOptions"
                   :key="option.value"
                   class="p-1 rounded-[5px] hover:bg-[#363636] text-left"
-                  @click.stop="showRowOptions = false"
+                  @click.stop="() => onClickOfOption(option.label, chain.id)"
                 >
                   {{ option.label }}
                 </button>
@@ -94,24 +100,15 @@ useClickOutside(showRowOptions_menu, () => {
 </template>
 
 <style scoped>
-.table-container {
-  border: 1px solid #363636;
-}
-
 th,
 td {
   padding: 15px;
   text-align: left;
 }
 
-thead {
-  border-bottom: 1px solid #363636;
-}
-
 th {
   font-size: 12px;
   font-weight: 400;
-  color: #8d8d8d;
 }
 
 td {
