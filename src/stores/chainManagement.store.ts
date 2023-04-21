cf3ab49112b764eb64d92c0de5f85b10e64e8e4e
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-04-21 18:06:00 +0530

    implement a getter to know if chains are present

--- src/stores/chainManagement.store.ts
+++ src/stores/chainManagement.store.ts
@@ -6,6 +6,11 @@ const useChainManagementStore = defineStore('chain-management', {
   state: () => ({
     chains: [],
   }),
+  getters: {
+    areChainsEmpty: ({ chains }) => {
+      return chains.length === 0
+    },
+  },
   actions: {
     async getAppChains(appId: string) {
       const { chains } = (await getChains(appId)).data
