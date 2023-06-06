e97b2a66a659934db94995ed8c57ede5416c429c
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-06 15:35:39 +0530

    show ui mode

--- src/components/app-configure/auth/WebWallet.vue
+++ src/components/app-configure/auth/WebWallet.vue
@@ -11,6 +11,7 @@ import { updateApp } from '@/services/gateway.service'
 import { useAppsStore, type Theme } from '@/stores/apps.store'
 import { useLoaderStore } from '@/stores/loader.store'
 import { useAppId } from '@/use/getAppId'
+import { walletUIModes } from '@/utils/constants'
 import { isValidUrl } from '@/utils/validation'
 
 const appsStore = useAppsStore()
@@ -20,6 +21,9 @@ const toast = useToast()
 const app = appsStore.app(appId)
 const wallet = app.auth.wallet
 const isEdited = ref(false)
+const choosenWalletUIMode = walletUIModes.find(
+  (mode) => mode.value === app.walletMode
+)?.label
 
 type ThemeData = {
   label: string
@@ -84,32 +88,52 @@ async function handleSave() {
 <template>
   <section name="web-wallet">
     <SettingCard>
-      <template #title>Wallet</template>
+      <template #title>Arcana Wallet</template>
       <form @submit.prevent="handleSave">
         <VStack direction="column" gap="1rem">
-          <VStack direction="column" gap="1rem" class="flex-grow">
-            <h3>Website Domain</h3>
-            <div class="text-lg font-normal text-grey">
-              Secure Arcana wallet by restricting to load only in the context of
-              the specified app domain. Arcana uses frame-ancestors CSP to
-              restrict domains.
-              <a
-                href="https://developer.mozilla.org/en-US/docs/web/http/headers/content-security-policy#frame-ancestors"
-                target="_blank"
+          <VStack>
+            <VStack direction="column" gap="1rem" class="flex-grow">
+              <h3>Website Domain</h3>
+              <div class="text-lg font-normal text-grey">
+                Secure Arcana wallet by restricting to load only in the context
+                of the specified app domain. Arcana uses frame-ancestors CSP to
+                restrict domains.
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/web/http/headers/content-security-policy#frame-ancestors"
+                  target="_blank"
+                >
+                  READ MORE
+                </a>
+              </div>
+              <VStack
+                class="space-x-3 flex max-[1200px]:flex-col max-[1200px]:space-x-0 max-[1200px]:space-y-5"
               >
-                READ MORE
-              </a>
-            </div>
-            <VTextField
-              v-model.trim="walletWebsiteDomain"
-              class="web-wallet-input"
-              :icon="walletWebsiteDomain ? CloseIcon : ''"
-              :message-type="isEdited && !isValidWebsiteDomain() ? 'error' : ''"
-              message="Invalid website domain - must be a valid url"
-              clickable-icon
-              @icon-clicked="clearWebsiteDomain()"
-              @blur="isEdited = true"
-            />
+                <VStack direction="column" class="flex-1 space-y-2">
+                  <VTextField
+                    v-model.trim="walletWebsiteDomain"
+                    :icon="walletWebsiteDomain ? CloseIcon : ''"
+                    :message-type="
+                      isEdited && !isValidWebsiteDomain() ? 'error' : ''
+                    "
+                    :no-message="!(isEdited && !isValidWebsiteDomain())"
+                    message="Invalid website domain - must be a valid url"
+                    clickable-icon
+                    label="Enter Domain URL"
+                    @icon-clicked="clearWebsiteDomain()"
+                    @blur="isEdited = true"
+                  />
+                </VStack>
+                <VStack direction="column" class="flex-1 space-y-2">
+                  <VTextField
+                    :no-message="true"
+                    class="max-w-[10rem]"
+                    :disabled="true"
+                    label="Chosen UI Mode"
+                    :model-value="choosenWalletUIMode"
+                  />
+                </VStack>
+              </VStack>
+            </VStack>
           </VStack>
           <ConfigureActionButtons
             :save-disabled="hasSameValuesInStore() || !isValidWebsiteDomain()"
@@ -127,9 +151,5 @@ async function handleSave() {
   font-size: 1rem;
   font-weight: 500;
   color: var(--text-grey);
-}
-
-.web-wallet-input {
-  max-width: 24rem;
 }
 </style>
