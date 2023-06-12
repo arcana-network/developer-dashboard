9688f204878f42b8cafb8e6cb29c1b983d2a5f6e
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-12 14:07:11 +0530

    create gasless homepage

---
+++ src/pages/AppGasless.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+import GaslessStep1Icon from '@/assets/iconography/gasless-step-1-icon.svg'
+import GaslessStep2Icon from '@/assets/iconography/gasless-step-2-icon.svg'
+import GaslessStep3Icon from '@/assets/iconography/gasless-step-3-icon.svg'
+import GaslessStep4Icon from '@/assets/iconography/gasless-step-4-icon.svg'
+
+const steps = [
+  {
+    indexLabel: '01',
+    icon: GaslessStep1Icon,
+    label: 'Setup Gas Tanks',
+  },
+  {
+    indexLabel: '02',
+    icon: GaslessStep2Icon,
+    label: 'Deposit Crypto to your Gas Tanks',
+  },
+  {
+    indexLabel: '03',
+    icon: GaslessStep3Icon,
+    label: 'Deposit Crypto to your Gas Tanks',
+  },
+  {
+    indexLabel: '04',
+    icon: GaslessStep4Icon,
+    label: 'Enable the Gas Tanks',
+  },
+]
+</script>
+
+<template>
+  <h1>Gasless Transactions</h1>
+  <p>
+    Use this interface to setup gas tanks that will sponsor the gas fees for
+    transactions performed by users of your dApp. You may also sponsor all
+    transactions or only specific functions within your app's contracts by
+    setting up the whitelists. Please note that a new client ID will be created
+    if you enable this feature. The client ID will remain even if you decide to
+    stop supporting gasless transactions in the future.
+  </p>
+  <div
+    class="flex max-[1350px]:flex-col justify-between bg-[#1F1F1F] border-[1px] border-[#363636] p-4 rounded-[10px] max-[1350px]:w-[300px]"
+  >
+    <div
+      v-for="(step, index) in steps"
+      :key="step.indexLabel"
+      after="''"
+      class="flex flex-column items-start space-y-2 flex-1 max-[1350px]:min-h-[150px]"
+      :class="[
+        index < 3
+          ? 'relative after:content-normal after:left-[65%] after:top-[70%] max-[1350px]:after:left-4 max-[1350px]:after:top-[50%] max-[1350px]:after:h-[70px] max-[1350px]:after:w-[1px] after:border-dotted after:border-[1px] after:absolute after:w-1/3'
+          : '',
+      ]"
+    >
+      <span class="text-[#8D8D8D] text-sm ml-[10px]">
+        {{ step.indexLabel }}
+      </span>
+      <div class="flex items-center space-x-2 w-2/3">
+        <img :src="step.icon" class="w-10 h-10" />
+        <span class="text-sm">{{ step.label }}</span>
+      </div>
+    </div>
+  </div>
+</template>
