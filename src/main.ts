37264a63f48d1df2c2fc23fd5005b9bd1a4953fa
muktanshumishra24 <muktanshumishra24@gmail.com>
2024-05-30 11:49:47 +0530

    Design Tweaks

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import constants from '@/utils/constants'
 
 import 'vue3-circle-progress/dist/circle-progress.css'
 import '@/components/lib/styles.css'
-import '@/assets/fonts/nohemi.css'
 
 const sentryConfig = constants.sentry
 
