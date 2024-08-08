6089ee8c4eaf9903da45d5643fad827912f04e8f
karthikdurai <iamkarthiksiddarth@gmail.com>
2024-08-08 08:24:51 +0530

    add emailer UI and toggle btn

--- src/components/app-configure/general/AppBrandingNew.vue
+++ src/components/app-configure/general/AppBrandingNew.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+
+import EmailerBranding from '@/components/app-configure/general/EmailerBranding.vue'
 import WalletBranding from '@/components/app-configure/general/WalletBranding.vue'
+
+const activeTab = ref('wallet')
+
+function isActiveTab(tab) {
+  console.log(tab, activeTab.value === tab, 'activeTab.value === tab')
+  return activeTab.value === tab
+}
 </script>
 
 <template>
@@ -11,5 +21,30 @@ import WalletBranding from '@/components/app-configure/general/WalletBranding.vu
       urna porttitor rhoncus dolor purus non.
     </p>
   </div>
-  <WalletBranding />
+  <div class="border-b-[1px] border-[#DCDCDC] flex flex-row gap-4">
+    <button
+      class="text-sm pb-1"
+      :class="[
+        isActiveTab('wallet')
+          ? 'text-[#1D2A31] border-b-[1px] border-[#1D2A31] font-semibold'
+          : 'text-[#989898] font-normal',
+      ]"
+      @click="activeTab = 'wallet'"
+    >
+      Wallet Branding
+    </button>
+    <button
+      class="text-sm pb-1"
+      :class="[
+        isActiveTab('emailer')
+          ? 'text-[#1D2A31] border-b-[1px] border-[#1D2A31] font-semibold'
+          : 'text-[#989898] font-normal',
+      ]"
+      @click="activeTab = 'emailer'"
+    >
+      Emailer Branding
+    </button>
+  </div>
+  <WalletBranding v-if="activeTab === 'wallet'" />
+  <EmailerBranding v-if="activeTab === 'emailer'" />
 </template>
